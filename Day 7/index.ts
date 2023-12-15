e70abad0f24e58158b4a21ee889f23c47e633ca0
Eduardo Donoso <eduardo@sunriseintegration>
2023-12-15 15:37:55 -0800

    finish day 7

--- Day 7/index.ts
+++ Day 7/index.ts
@@ -2,69 +2,135 @@ const problemHandler = require('../utils/problemHandler.js')
 
 problemHandler(__filename, run, ['data'])
 
-function run(data: string[]){
-    console.log('data', data)
-    const hands = data.map(d => d.split(' ').map(i => i))
-    console.log('hands', hands)
-    const handTypes = hands.map(hand => {
-        return getHandType(hand)
-    })
-    console.log('handTypes',handTypes)
+const cardStrength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+const cardStrengthJokers = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
 
-    handTypes.sort((a,b) => {
-        if(a.strength < b.strength) return 1
-        if(a.strength > b.strength) return -1
-        //Iterate over hand to find best single card
-        for(let i=0; i<a.hand.length; i++){
+interface Hand {
+    counts: CardCount[],
+    strength: number,
+    wildStrength: number,
+    hand: [],
+    bid: number
+}
+
+interface CardCount {
+    count: number,
+    card: string
+}
+
+function run(data: string[]) {
+    let hands: Hand[] = data.map(d => d.split(' ')
+        .map(i => i))
+        .map(hand => {
+            return getHandData(hand)
+        })
+
+    // Problem 1
+    hands = sortHands(hands, cardStrength)
+    const totalWinnings = hands.reduce((prev, curr, i) => {
+        return prev + curr.bid * (hands.length - i)
+    }, 0)
+
+
+    // Problem 2
+    hands = sortHands(hands, cardStrengthJokers, 'wildStrength')
+    const totalWildWinnings = hands.reduce((prev, curr, i) => {
+        return prev + curr.bid * (hands.length - i)
+    }, 0)
+
+    return [totalWinnings, totalWildWinnings]
+}
+
+function sortHands(handTypes: any[], cardStrength: string[], field: string = 'strength'): Hand[] {
+    handTypes.sort((a, b) => {
+        if (a[field] < b[field]) return 1
+        if (a[field] > b[field]) return -1
+        //Iterate over hand to find best card in hand position
+        for (let i = 0; i < a.hand.length; i++) {
             const cardA = a.hand[i]
             const cardB = b.hand[i]
-            if(cardA == cardB) continue
+            if (cardA == cardB) continue
             const cardStrengthA = cardStrength.findIndex(i => i == cardA)
             const cardStrengthB = cardStrength.findIndex(i => i == cardB)
-            if(cardStrengthA < cardStrengthB) return -1
-            if(cardStrengthA > cardStrengthB) return 1
+            if (cardStrengthA < cardStrengthB) return -1
+            if (cardStrengthA > cardStrengthB) return 1
         }
         return 0
     })
-    console.log('handTypes',handTypes)
-
-    const totalWinnings = handTypes.reduce((prev, curr, i) => {
-        return prev + curr.bid * (handTypes.length - i)
-    }, 0)
-    console.log('totalWinnings',totalWinnings)
+    return handTypes
 }
 
-function getHandType(hand: any){
-    let cardCounts: any = { counts:{}}
-    for(let i = 0; i < hand[0].length; i++){
-        const cardInHand = hand[0][i]
-        if(!cardCounts.counts.hasOwnProperty(cardInHand)) cardCounts.counts[cardInHand] = 0
-        cardCounts.counts[cardInHand] = cardCounts.counts[cardInHand] + 1
+function getHandData(data: any) {
+    const counts = getHandCounts(data)
+    return {
+        hand: data[0],
+        bid: data[1],
+        strength: getHandStrength(counts),
+        wildStrength: getWildHandStrength(counts),
+        counts: counts
     }
-    console.log('cardCounts',cardCounts)
-
-   cardCounts.strength = checkCount(0, cardCounts.counts)
-   cardCounts.hand = hand[0]
-   cardCounts.bid = hand[1]
-   console.log('cardCounts',cardCounts)
-
-   return cardCounts
 }
 
-function checkCount(highestCount: number, cardCounts: {}){
-    for(const [card, count] of Object.entries(cardCounts)){
-        console.log(card, count)
-        //Check for 2 of a kind, full house
-        if(count == 3 && highestCount == 2) return 3.5
-        if(count == 2 && highestCount == 3) return 3.5
-        if(count == 2 && highestCount == 2)return 2.5
-          // @ts-ignore
-        if(count < highestCount) continue
-          // @ts-ignore
-        highestCount = count            
+function getHandCounts(data: any) {
+    let handCounts = new Map()
+    for (let i = 0; i < data[0].length; i++) {
+        const cardInHand: string = data[0][i]
+        handCounts.set(cardInHand, (handCounts.get(cardInHand) || 0) + 1)
+    }
+    let sortedCounts: { count: number, card: string }[] = [];
+    for (const [card, count] of handCounts.entries()) {
+        sortedCounts.push({card, count: Number(count)})
+    }
+    sortedCounts.sort((a, b) => {
+        if (a.count < b.count) return 1
+        if (a.count > b.count) return -1
+        return 0
+    })
+    return sortedCounts;
+}
+
+function getHandStrength(cardCounts: CardCount[]) {
+    let highestCount: number = 0
+    for (let i = 0; i < cardCounts.length; i++) {
+        const cardCount = cardCounts[i]
+        if (cardCount.count == 2 && highestCount == 3) return 3.5 // Full house
+        if (cardCount.count == 2 && highestCount == 2) return 2.5 // Two pair
+        if (cardCount.count < highestCount) break // Array is sorted so we can bail out
+        highestCount = cardCount.count // Highest of a kind
     }
     return highestCount
 }
 
-const cardStrength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-const handStrength = ['5','4','F','3','T','2','1']
+function getWildHandStrength(cardCounts: CardCount[]) {
+
+    const jokerIndex = cardCounts.findIndex(i => i.card === 'J')
+    const jokerCount: CardCount = jokerIndex >= 0 ? cardCounts[jokerIndex] : {count: 0, card: 'J'}
+
+    for (let i = 0; i < cardCounts.length; i++) {
+        const cardCount = cardCounts[i]
+        const withWildCount = cardCount.count + jokerCount.count
+
+        if (cardCount.card == 'J' && cardCounts.length == 1) return cardCount.count // Oops...all Jokers
+        if (cardCount.card == 'J') continue // Check next card
+
+        if (withWildCount > 3) return withWildCount // 5 and 4 of a kind
+
+        if (cardCount.count == 3 && cardCounts[i + 1].count == 2) return 3.5 // Full house
+        if (cardCount.count + jokerCount.count == 3 && cardCounts[i + 1].count == 2) return 3.5 // Full house
+
+        if (jokerCount.count == 2 && cardCount.count + 1 == 3 && cardCounts[i + 1].count + 1 == 2) return 3.5 // Full house
+
+        if (withWildCount == 3) return withWildCount // Three of a kind
+
+        if (cardCount.count == 2 && cardCounts[i + 1].count == 2) return 2.5 // Two pair
+        if (cardCount.count == 2 && cardCounts[i + 1].count + jokerCount.count == 2) return 2.5 // Two pair
+
+        if (jokerCount.count == 1 && cardCount.count == 2 && cardCounts[i + 1].count + 1 == 2) return 2.5 // Two pair
+        if (jokerCount.count == 2 && cardCount.count + 1 == 2 && cardCounts[i + 1].count + 1 == 2) return 2.5 // Two pair
+
+        return withWildCount
+    }
+    return 1
+}
+
+
